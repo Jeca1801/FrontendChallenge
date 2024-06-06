216aee579fabd367fb3eca37f34e3db885740490
EX14186 <jesper.c.larsson@kronofogden.se>
2024-06-06 12:29:30 +0200

    feat: add intial structure for countdown-date-input

--- src/app/shared/countdown-date-input/countdown-date-input.component.ts
+++ src/app/shared/countdown-date-input/countdown-date-input.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core'
+import {Component, EventEmitter, Input, Output} from '@angular/core'
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-countdown-date-input',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './countdown-date-input.component.html',
 })
-export class CountdownDateInputComponent {}
+export class CountdownDateInputComponent {
+  @Input({ required: true }) control!: FormControl
+  @Input({ required: true }) label!: string
+  @Output() changeEvent: EventEmitter<void> = new EventEmitter<void>()
+
+  onChange() {
+    this.changeEvent.emit()
+  }
+}
