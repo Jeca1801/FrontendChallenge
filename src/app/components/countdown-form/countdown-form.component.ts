6159dff24c64fa3455b6e4dbfa693a6cc871e0e1
EX14186 <jesper.c.larsson@kronofogden.se>
2024-06-06 14:21:58 +0200

    feat add logic for service and countdown form

--- src/app/components/countdown-form/countdown-form.component.ts
+++ src/app/components/countdown-form/countdown-form.component.ts
@@ -1,9 +1,56 @@
 import { Component } from '@angular/core'
+import {Subject, takeUntil} from "rxjs";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {CountdownFormService} from "../../shared/service/countdown-form.service";
+import {CountdownFormModel} from "../../shared/model/countdown-form.model";
+import {CountdownDateInputComponent} from "../../shared/countdown-date-input/countdown-date-input.component";
+import {CountdownTitleInputComponent} from "../../shared/countdown-title-input/countdown-title-input.component";
 
 @Component({
   selector: 'app-countdown-form',
   standalone: true,
-  imports: [],
+  imports: [CountdownTitleInputComponent, CountdownDateInputComponent, ReactiveFormsModule],
   templateUrl: './countdown-form.component.html',
 })
-export class CountdownFormComponent {}
+export class CountdownFormComponent {
+  private destroy$: Subject<void> = new Subject<void>()
+  countdownForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    date: new FormControl('', Validators.required),
+  })
+
+  constructor(private countdownFormService: CountdownFormService) {}
+
+  ngOnInit(): void {
+    this.countdownFormService.getFormState()
+
+    this.countdownFormService.formState$.pipe(takeUntil(this.destroy$)).subscribe(state => {
+      if (state) {
+        this.countdownForm.patchValue({
+          title: state.title,
+          date: new Date(state.date).toISOString().slice(0, 10),
+        })
+      }
+    })
+  }
+
+  onDateChange(): void {
+    if (this.countdownForm.valid) {
+      this.onSubmit()
+    }
+  }
+
+  onBlur(): void {
+    if (this.countdownForm.valid) {
+      this.onSubmit()
+    }
+  }
+
+  onSubmit(): void {
+    const formValue: CountdownFormModel = {
+      title: this.countdownForm.get('title')?.value || '',
+      date: this.countdownForm.get('date')?.value || new Date(),
+    }
+    this.countdownFormService.setFormState(formValue)
+  }
+}
--- src/app/shared/service/countdown-form.service.ts
+++ src/app/shared/service/countdown-form.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@angular/core'
+import {BehaviorSubject, Observable} from "rxjs";
+import {CountdownFormModel} from "../model/countdown-form.model";
 
 @Injectable({
   providedIn: 'root',
 })
-export class CountdownFormService {}
+export class CountdownFormService {
+  private formState: BehaviorSubject<CountdownFormModel | null> = new BehaviorSubject<CountdownFormModel | null>({
+    title: 'Time to Midsummer  Eve',
+    date: new Date(2024, 5, 21),
+  })
+  formState$: Observable<CountdownFormModel | null> = this.formState.asObservable()
+
+  setFormState(formState: CountdownFormModel): void {
+    this.formState.next(formState)
+    localStorage.setItem('formState', JSON.stringify(formState))
+  }
+
+  getFormState(): void {
+    const savedState: string | null = localStorage.getItem('formState')
+    if (savedState) {
+      this.formState.next(JSON.parse(savedState))
+    }
+  }
+}
